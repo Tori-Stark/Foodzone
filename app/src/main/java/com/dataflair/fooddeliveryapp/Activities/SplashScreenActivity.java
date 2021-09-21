aa8c5a17b977ee46c598e401a06f2514b8131a07
Tori-Stark <61185176+Tori-Stark@users.noreply.github.com>
2021-09-21 19:23:10 +0300

    Logo And splash screen

---
+++ app/src/main/java/com/dataflair/fooddeliveryapp/Activities/SplashScreenActivity.java
@@ -0,0 +1,20 @@
+package com.dataflair.fooddeliveryapp.Activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import com.dataflair.fooddeliveryapp.R;
+
+
+
+public class SplashScreenActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+
+
+
+    }
+}
